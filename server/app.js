636f6d00defcccd98bd6960a6b52210d005ac003
FUJII Hiroshi <fujii@acutus.co.jp>
2021-09-02 21:12:05 +0900

    add error handler

--- server/app.js
+++ server/app.js
@@ -3,10 +3,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+function faceErrorHandler (err, req, res, next) {
+  try {
+    res.setHeader('content-type', "text/plain");
+    res.status(err.statusCode).send(err.message);
+  } catch(err) {
+    next(err);
+  }
+}
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use(faceErrorHandler);
 
 module.exports = app;
