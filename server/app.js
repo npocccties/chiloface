7df56a474ba76651fd012d31e0c7b52abcfbb286
FUJII Hiroshi <fujii@acutus.co.jp>
2021-09-21 10:21:16 +0900

    change url

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', apiv1Router);
+app.use('/api/v1', apiv1Router);
 app.use(faceErrorHandler);
 
 module.exports = app;
